8f5f72cac938f4548acd8aa3e3bb586fa8aa25a3
koki3141 <100524649+koki3141@users.noreply.github.com>
2023-03-07 01:14:00 +0900

    Update dl.py

--- dl.py
+++ dl.py
@@ -9,8 +9,6 @@ API_KEY = '' # 自身の API キーを指定
 input_string = pyperclip.paste()
 output_string  = re.sub(r"\r\n|\r|\n", " ", input_string).replace("  ", " ")
 
-#pyperclip.copy(output_string)
-
 text=output_string
 source_lang = 'EN'
 target_lang = 'JA'
@@ -22,6 +20,8 @@ try:
     result = translator.translate_text(text, source_lang=source_lang, target_lang=target_lang)
     print(result)
 except:
+
+#api制限の例外処理
     print('利用制限がかかりました。')
     pyperclip.copy(output_string)
     print(output_string)
